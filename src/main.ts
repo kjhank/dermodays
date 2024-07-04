4b9b19a0dce3913808839e2003e05168626fad25
Krzysztof Hankiewicz @kjhank <kjhank@gmail.com>
2024-07-04 07:36:21 +0200

    add scrolling logic for cases where anchors fail; update asset paths to relative

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import {
   CartButton, VimeoPlayer, PatternSvg, PlayIcon, PromoCarousel, PromoItem,
-  ScrollButton,
-  DozProduct,
-  ChevronSvg,
+  ScrollButton, DozProduct, ChevronSvg,
 } from './scripts/components';
 import './styles/style.css';
 
@@ -15,3 +13,27 @@ customElements.define('pattern-svg', PatternSvg);
 customElements.define('scroll-button', ScrollButton);
 customElements.define('doz-product', DozProduct);
 customElements.define('chevron-svg', ChevronSvg);
+
+const anchorLinks = document.querySelectorAll('a[href^="#"]');
+anchorLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const id = link.getAttribute('href');
+
+    const navHeight = link.closest('nav')?.getBoundingClientRect().height ?? 0;
+
+    if (!id) return;
+
+    const targetNode = document.querySelector(id);
+    if (!targetNode) return;
+
+    const scrollDistance = (targetNode as HTMLElement).offsetTop - navHeight;
+
+    window.scroll({
+      top: scrollDistance,
+      behavior: 'smooth',
+    });
+
+    window.history.pushState({}, '', id);
+  });
+});
